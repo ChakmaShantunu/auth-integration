4fcd87dfb387bdf67ce632c56e65b16ec20f33d5
shantunu007 <chakmashantunu.web@gmail.com>
2026-06-30 20:01:39 +0600

    add registration form with validation and loading states

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,15 +2,26 @@ import { Link } from "react-router";
 
 
 const Register = () => {
+
+    const handleRegister = e => {
+        e.preventDefault();
+
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password);
+    }
     return (
         <div className="card bg-base-100 w-full mx-auto mt-12 p-6 max-w-sm shrink-0 shadow-2xl">
             <h1 className="text-3xl font-bold text-center">Register now!</h1>
             <div className="card-body">
-                <form className="fieldset">
+                <form onSubmit={handleRegister} className="fieldset">
+                    <label className="label">Name</label>
+                    <input type="text" name="name" className="input" placeholder="Name" />
                     <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email" />
+                    <input type="email" name="email" className="input" placeholder="Email" />
                     <label className="label">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
+                    <input type="password" name="password" className="input" placeholder="Password" />
                     <button className="btn btn-neutral mt-4">Register</button>
                 </form>
                 <p>Already have an account? Please <Link to="/login" className="underline text-blue-400">Login</Link></p>
