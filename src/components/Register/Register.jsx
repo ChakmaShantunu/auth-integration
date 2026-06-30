347c9b69becffbb22746512ec660268bddb1ae77
shantunu007 <chakmashantunu.web@gmail.com>
2026-06-30 19:49:55 +0600

    complete register & login form for credintails

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router";
 
 
 const Register = () => {
     return (
-        <div>
-            <h2 className="text-3xl">this is register</h2>
+        <div className="card bg-base-100 w-full mx-auto mt-12 p-6 max-w-sm shrink-0 shadow-2xl">
+            <h1 className="text-3xl font-bold text-center">Register now!</h1>
+            <div className="card-body">
+                <form className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" className="input" placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" className="input" placeholder="Password" />
+                    <button className="btn btn-neutral mt-4">Register</button>
+                </form>
+                <p>Already have an account? Please <Link to="/login" className="underline text-blue-400">Login</Link></p>
+            </div>
         </div>
     );
 };
